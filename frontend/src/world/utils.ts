8e4682fb3bd168fcb08949ecc7747e964f0a9788
zvonimirfras <zvonimir@zvonimirfras.com>
2021-02-24 19:45:30 -0500

    Improve mutate function

--- frontend/src/world/utils.ts
+++ frontend/src/world/utils.ts
@@ -1,13 +1,26 @@
 import { Vector3 } from "@babylonjs/core";
 
 export class Utils {
-	static mutate(initialValue: number | Vector3, randomization: number) : any {
+	static mutate(initialValue: number | Vector3 | any, randomization: number) : any {
 		if (initialValue instanceof Vector3) {
 			return new Vector3(
 				this.mutate(initialValue.x, randomization),
 				this.mutate(initialValue.y, randomization),
 				this.mutate(initialValue.z, randomization)
 			);
+		}
+
+		if (typeof initialValue === 'object') {
+			const returnValue: any = {};
+			
+			Object.keys(initialValue).forEach(element => {
+				let randomizationRate = randomization;
+				if (typeof initialValue[element] === 'number') {
+					randomizationRate = initialValue[element] * randomization;
+				}
+				returnValue[element] = Utils.mutate(initialValue[element], randomizationRate);
+			});
+			return returnValue;
 		}
 
 		const sign = Math.random() > 0.5 ? -1 : 1;

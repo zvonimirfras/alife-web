83b666ef8e81a0ebfeb4a81db3f41f073a5e9adc
zvonimirfras <zvonimir@zvonimirfras.com>
2021-03-07 11:57:08 -0500

    Add initial help modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,18 @@ import {
 	Vector3
 } from '@babylonjs/core';
 import React, { useEffect, useState } from 'react';
-import { Menu32, Play32, Pause32 } from '@carbon/icons-react';
+import {
+	Menu32,
+	Play32,
+	Pause32,
+	Help32,
+	Help16
+} from '@carbon/icons-react';
 
 import './App.scss';
 import { Controls } from './Controls';
 import { SceneComponent } from './SceneComponent';
-import { Button } from 'carbon-components-react';
+import { Button, Checkbox, Modal } from 'carbon-components-react';
 import { World } from './world/world';
 import { StatisticsView } from './StatisticsView';
 
@@ -41,11 +47,19 @@ function App() {
 	const [isMenuOpen, setIsMenuOpen] = useState(true);
 	const [shouldSceneResize, setShouldSceneResize] = useState(false);
 	const [shouldRunSimulation, setShouldRunSimulation] = useState(true);
+	const [shouldShowModal, setShouldShowModal] = useState(false);
+	const [shouldHideHelpModalAtStart, setShouldHideHelpModalAtStart] = useState(false);
 
 	const toggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen);
 		setTimeout(() => setShouldSceneResize(true), 150); // after the animation
 	};
+
+	useEffect(() => {
+		const hideHelpModalAtStart = JSON.parse(localStorage.getItem('hideHelpModalAtStart') || 'false');
+		setShouldHideHelpModalAtStart(hideHelpModalAtStart);
+		setShouldShowModal(!hideHelpModalAtStart);
+	}, []);
 
 	useEffect(() => {
 		if (!world.current) {
@@ -79,12 +93,71 @@ function App() {
 				className={'scene ' + (!isMenuOpen ? 'expand-scene' : '')}
 				world={world} />
 			<Button
+				className={`help-button${!isMenuOpen ? ' menu-hidden' : ''}`}
+				kind='ghost'
+				renderIcon={Help32}
+				iconDescription='Help'
+				hasIconOnly
+				onClick={() => setShouldShowModal(true)} />
+			<Button
 				className='menu-button'
 				kind='secondary'
 				renderIcon={Menu32}
 				iconDescription='Menu'
 				hasIconOnly
 				onClick={toggleMenu} />
+			<Modal
+			open={shouldShowModal}
+			passiveModal
+			onRequestClose={() => setShouldShowModal(false)}
+			className='hello-modal'
+			modalHeading="Welcome to the simulation!">
+				<p>
+					ALife simulation enables you to experiment with starting parameters of the artificial world and see how
+					they influence development of its inhabitants and the stability of the system.
+				</p>
+				<p>
+					Initial parameters for a cohort (Plants, Herbivores, or Predators) are randomized around the values you
+					put in the boxes. Spread depends on the mutation rate. This makes a more natural, diverse, world to start with.
+				</p>
+				<p>
+					Their decision making is entirely instinctual and pre-coded. Behaviour changes with the environment they are
+					in and their immediate surroundings. They inherit their parents' genetics with mutation rate dependant on the
+					mutation rate.
+				</p>
+				<h4>Plants</h4>
+				<p>
+					Plants provide source of energy into the world. They grow with time and increase the energy with their size.
+					When eaten, they decrease in size and energy transfers to the inhabitant that ate it.
+					They periodically try to reproduce.
+				</p>
+				<h4>Herbivores</h4>
+				<p>
+					Herbivores consume plants for energy and will go after them if they are low on energy. They use energy to
+					move around and to reproduce.
+					If there are predators near by, they try to run away. If they are capable, they look for a partner and try
+					to reproduce.
+				</p>
+				<h4>Predators</h4>
+				<p>
+					Predators chase and consume herbivores for energy. When they catch a herbivore, they stun them and make them
+					paralized for a little while. That helps them feed. They use energy to
+					move around and to reproduce. If they are capable, they look for a partner and try
+					to reproduce.
+				</p>
+				<em>
+					Press <Help16 /> on the top-right to show this modal again.
+				</em>
+				<Checkbox
+					id='show-at-start'
+					labelText='Show at start'
+					className='show-at-start-checkbox'
+					onChange={(checked) => {
+						setShouldHideHelpModalAtStart(!checked);
+						localStorage.setItem('hideHelpModalAtStart', JSON.stringify(!checked));
+					}}
+					checked={!shouldHideHelpModalAtStart} />
+			</Modal>
 		</div>
 	);
 }
